752765a4359b4cc936e7324d3bfd4a851feb3bd7
stahlgit <stahlpeter2@gmail.com>
2023-10-16 13:29:20 +0200

    Initial commit

---
+++ todo_project/todo_app/views.py
@@ -0,0 +1,9 @@
+# from django.shortcuts import render
+from rest_framework import viewsets
+from .models import TodoItem
+from .serializers import TodoItemSerializer
+
+
+class TodoItemViewSet(viewsets.ModelViewSet):
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemSerializer
